c0e5f64f66ba474fe9aaa8ecbe24665d292446c8
Cole Gawin <colegawin@gmail.com>
2022-02-13 14:37:55 -0600

    import fonts via link tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,6 +41,13 @@ export default function App(props: AppProps) {
 
         <link rel={"manifest"} href={"/manifest.json"} />
         <link rel={"icon"} href={"/favicon.png"} />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"anonymous"} />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Spline+Sans:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <ChakraProvider resetCSS theme={extendTheme(theme)}>
         <props.Component {...props.pageProps} />
